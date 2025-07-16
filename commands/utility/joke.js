646c935c585dd785673df38f6cbd90b77e830399
Pipetr <faaa1901@gmail.com>
2025-07-16 17:46:28 -0400

    joke example completed

---
+++ commands/utility/joke.js
@@ -0,0 +1,29 @@
+const oneLinerJoke = require('one-liner-joke');
+
+
+// // Get a random joke
+// const joke = oneLinerJoke.getRandomJoke();
+// console.log(joke.body);
+
+// // Get a nerdy joke
+// const nerdyJoke = oneLinerJoke.getRandomJoke({ 'exclude_tags': ['dirty'] });
+// console.log(nerdyJoke.body);
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('joke')
+        .setDescription('Replies with a random joke!'),
+    async execute(interaction) {
+        // Get a random joke
+        const joke = oneLinerJoke.getRandomJoke();
+        // Reply with the joke
+        if (!joke) {
+            await interaction.reply('Sorry, I could not fetch a joke at the moment.');
+            return;
+        }
+        await interaction.reply(joke.body);
+    }
+    
+};
